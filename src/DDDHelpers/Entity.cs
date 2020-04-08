1acaed15f53b64b06a687940b4391386c6d4036c
HassanHashemi <HassanHashemi>
2020-04-08 19:57:08 +0430

    cleanup

--- src/DDDHelpers/Entity.cs
+++ src/DDDHelpers/Entity.cs
@@ -6,9 +6,5 @@ namespace Domain
     {
         public bool Deleted { get; protected set; }
         public DateTime CreateDate { get; protected set; }
-        public long CreatedBy { get; protected set; }
-
-        public void SetDeleted() => this.Deleted = true;
-        public void UndoDeleted() => this.Deleted = false;
     }
 }
