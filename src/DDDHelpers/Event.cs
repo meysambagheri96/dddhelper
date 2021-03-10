c03a5deac23e72ef0cc89fde6a6426e8dcf914ba
Hassan Hashemi <HassanHashemi@yahoo.com>
2021-03-10 14:12:50 +0330

    make event class more serializable

--- src/DDDHelpers/Event.cs
+++ src/DDDHelpers/Event.cs
@@ -4,8 +4,26 @@ namespace Domain
 {
     public class Event
     {
+        private string _eventName;
+
         public DateTime Timestamp { get; set; }
-        public virtual string EventName => GetType().FullName;
+        public virtual string EventName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_eventName))
+                {
+                    _eventName = this.GetType().FullName;
+                }
+
+                return _eventName;
+            }
+            set
+            {
+                // used when deserializing to `Domain.Event`
+                _eventName = value;
+            }
+        }
         public virtual bool MustPropagate { get; set; }
         public int Version { get; set; }
         public object EventMetadata { get; set; }
